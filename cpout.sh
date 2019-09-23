e1eaf870f55b66cb9547721c5204749ced6f6ab6
Jake Sprouse <sprouse@synapse.com>
2019-09-23 08:36:28 -0700

    Better creation of directories needed for dot files

--- cpout.sh
+++ cpout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # copies everything from the repo into their designated places
-cat map | awk '!/^[[:space:]]*#/ && !/^[[:space:]]*$/ { print "cp -R " $1 " " $2 }' | bash
+cat map | awk '!/^[[:space:]]*#/ && !/^[[:space:]]*$/ { print "cp -RTv " $1 " " $2 }' | bash
